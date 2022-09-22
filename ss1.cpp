432c64982863753a6d5ee83954278cd93abf1468
yehui2001 <365526026@qq.com>
2022-09-22 23:02:42 +0800

    6

--- ss1.cpp
+++ ss1.cpp
@@ -1,7 +1,32 @@
 #include<iostream>
+#include<cstring>
 using namespace std;
-int main()
+int myMax(int x,int y)
 {
-    cout<<"鎴戣崏浣犲"<<endl;
+    if(x>y) return x;
+    else  return y;
+}
+char *myMax(char *first,char *second)
+ {
+    if(strcmp(first,second)>0)  return first;
+    else return second;
+ }
+int option=1;
+int main()
+{   
+    
+    switch (option)
+    {
+    case 1:
+        //重载函数，用一个函数来对多个不同类型的变量进行运算。
+        cout<<myMax(1,2)<<endl;
+        cout<<myMax('a','T')<<endl;
+        break;
+    case 2:
+    default:
+        break;
+    }
+
+    cout<<""<<endl;
     return 0;
 }
